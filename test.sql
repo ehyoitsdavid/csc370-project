98f749ba444f6f2662197791b456b7634b4c9866
Merkl98 <159560820+Merkl98@users.noreply.github.com>
2024-06-07 11:13:54 -0700

    Updated test.sql

--- test.sql
+++ test.sql
@@ -9,7 +9,8 @@ CREATE TABLE `books` (
   `author` varchar(255) DEFAULT NULL,
   `course` varchar(255) DEFAULT NULL,
   `price` INT DEFAULT NULL,
-  status VARCHAR(10) DEFAULT 'for sale'
+  `status` VARCHAR(10) DEFAULT 'for sale' --Added `` to status. Is this correct? - alex
+    -- `subtextID` ?
 );
 
 
@@ -18,17 +19,25 @@ CREATE TABLE `users` (
   `vnum` VARCHAR(40) NOT NULL PRIMARY KEY,
   `email` varchar(40) DEFAULT NULL,
   `balance` INT DEFAULT NULL
+  -- `isAdmin` ?
 );
 
-
+-- Change to transactions
 CREATE TABLE `records` (
   `date_sold` VARCHAR(40) DEFAULT NULL,
-  `ISBN` VARCHAR(15) NOT NULL,
+  `ISBN` VARCHAR(15) NOT NULL, --potentially remove?
   `seller_vnum` varchar(40) NOT NULL,
   `buyer_vnum` varchar(40) NOT NULL,
   `price_sold` INT DEFAULT NULL,
+  -- `recordID` ?
+    -- `subtextID` ?
   FOREIGN KEY (ISBN) REFERENCES books(ISBN),
     FOREIGN KEY (seller_vnum) REFERENCES users(vnum),
     FOREIGN KEY (buyer_vnum) REFERENCES users(vnum)
 );
+
+-- CREATE TABLE `records` (
+  --`ISBN` VARCHAR(40) DEFAULT NULL,
+  --`subtextID` varchar(40) NOT NULL,
+
 
