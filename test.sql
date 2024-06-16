cac6851c5ee57820bc80ae473e66a87bf9d26365
David Hu <106898439+ehyoitsdavid@users.noreply.github.com>
2024-06-16 13:13:41 -0700

    Updated code according to ERD

--- test.sql
+++ test.sql
@@ -1,43 +1,75 @@
-DROP TABLE IF EXISTS `records`; 
-DROP TABLE IF EXISTS `users`;
-DROP TABLE IF EXISTS `books`;
+-- DROP TABLE `records`;
+-- DROP TABLE `books`;
+-- DROP TABLE `users`;
 
+CREATE TABLE `authors` (
+    `author_id` INT NOT NULL PRIMARY KEY,
+    `author_name` VARCHAR(20) DEFAULT NULL
+);
+
+CREATE TABLE `auth_book` (
+    `ISBN` VARCHAR(15) NOT NULL PRIMARY KEY,
+    `author_id` INT NOT NULL,
+    `edition` VARCHAR(15) NOT NULL,
+    `year` INT NOT NULL,
+    FOREIGN KEY (author_id) REFERENCES authors(author_id)
+);
 
 CREATE TABLE `books` (
   `title` VARCHAR(50) DEFAULT NULL,
   `ISBN` VARCHAR(15) NOT NULL PRIMARY KEY,
-  `author` varchar(255) DEFAULT NULL,
-  `course` varchar(255) DEFAULT NULL,
-  `price` INT DEFAULT NULL,
-  `status` VARCHAR(10) DEFAULT 'for sale' -- Added `` to status. Is this correct? - alex
-    -- `subtextID` ?
+  `type` VARCHAR(15) NOT NULL,
+   FOREIGN KEY (ISBN) REFERENCES auth_book(ISBN)
+);
+
+CREATE TABLE `book_price`(
+    `ISBN` VARCHAR(15) NOT NULL,
+    `price` INT NOT NULL,
+    FOREIGN KEY (ISBN) REFERENCES auth_book(ISBN)
+);
+
+CREATE TABLE `course` (
+    `course_id` VARCHAR(10) NOT NULL PRIMARY KEY,
+    `course_name` VARCHAR(30) NOT NULL,
+    `field_of_study` VARCHAR(30) DEFAULT NULL
+);
+
+CREATE TABLE `required_text` (
+    `ISBN` VARCHAR(15) NOT NULL,
+    `course_id` VARCHAR(10) NOT NULL,
+    PRIMARY KEY (ISBN, course_id),
+    FOREIGN KEY (ISBN) REFERENCES auth_book(ISBN),
+    FOREIGN KEY (course_id) REFERENCES course(course_id)
 );
 
 
-CREATE TABLE `users` (
+CREATE TABLE `user_account` (
+  `v_number` VARCHAR(10) NOT NULL PRIMARY KEY,
   `name` VARCHAR(40) DEFAULT NULL,
-  `vnum` VARCHAR(40) NOT NULL PRIMARY KEY,
   `email` varchar(40) DEFAULT NULL,
   `balance` INT DEFAULT NULL
   -- `isAdmin` ?
 );
 
 -- Change to transactions
-CREATE TABLE `records` (
+CREATE TABLE `transactions` (
+  `record_id` INT PRIMARY KEY NOT NULL,
   `date_sold` VARCHAR(40) DEFAULT NULL,
-  `ISBN` VARCHAR(15) NOT NULL, -- potentially remove?
-  `seller_vnum` varchar(40) NOT NULL,
-  `buyer_vnum` varchar(40) NOT NULL,
+  `ISBN` VARCHAR(15) NOT NULL,
+  `v_number` varchar(10) NOT NULL,
   `price_sold` INT DEFAULT NULL,
-  -- `recordID` ?
-    -- `subtextID` ?
   FOREIGN KEY (ISBN) REFERENCES books(ISBN),
-    FOREIGN KEY (seller_vnum) REFERENCES users(vnum),
-    FOREIGN KEY (buyer_vnum) REFERENCES users(vnum)
+  FOREIGN KEY (v_number) REFERENCES user_account(v_number)
 );
 
--- CREATE TABLE `records` (
-  -- `ISBN` VARCHAR(40) DEFAULT NULL,
-  -- `subtextID` varchar(40) NOT NULL,
+CREATE TABLE `price_record`(
+    `price_sold` INT PRIMARY KEY NOT NULL,
+    `seller_split` INT DEFAULT NULL
+);
 
-
+CREATE TABLE `book_sale`(
+    `ISBN` VARCHAR(10) NOT NULL,
+    `quantity` INT NOT NULL,
+    FOREIGN KEY (ISBN) REFERENCES books(isbn),
+    FOREIGN KEY (ISBN) REFERENCES transactions(isbn)
+)
