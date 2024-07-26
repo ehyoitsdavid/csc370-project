704da728178feecfa9be61f275a6a00064cae4f7
David Hu <106898439+ehyoitsdavid@users.noreply.github.com>
2024-07-25 19:53:21 -0700

    Simplify the triggers (but keep triggers needed for business logic)

--- triggers.sql
+++ triggers.sql
@@ -1,8 +1,6 @@
 -- DROP TRIGGER invalid_order;
 -- DROP TRIGGER balance_less_than_0;
 -- DROP TRIGGER not_in_inventory;
--- DROP TRIGGER dec_inventory;
--- DROP TRIGGER dec_balance;
 -- DROP TRIGGER total_error;
 -- When a user submits an order with total price 0 or negative
 CREATE TRIGGER invalid_order
@@ -46,64 +44,16 @@ BEGIN
     SELECT quantity
     INTO inventory_quantity
     FROM inventory
-    WHERE ISBN = NEW.ISBN;
+    WHERE ISBN = NEW.ISBN
+    FOR UPDATE;
     IF (NEW.quantity > inventory_quantity) THEN
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT =
                 'Error: the book you are trying to place order on is not in inventory';
     END IF;
-
-END;
--- After the transaction, deduct user balance, handle the case when the transaction fails due to multiple orders
-CREATE TRIGGER dec_balance
-    AFTER INSERT
-    ON `transactions`
-    FOR EACH ROW
-BEGIN
-    DECLARE previous_balance INT;
-    DECLARE new_balance INT;
-    SELECT balance
-    INTO previous_balance
-    FROM user_account
-    WHERE user_account.v_number = NEW.v_number;
-
-    SELECT balance - NEW.total_price
-    INTO new_balance
-    FROM user_account
-    WHERE user_account.v_number = NEW.v_number;
-
-    IF new_balance < 0 THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Error: Insufficient account balance; Cannot complete transaction.';
-
-    ELSE
-        UPDATE `user_account`
-        SET balance = new_balance
-        WHERE user_account.v_number = NEW.v_number;
-    END IF;
-END;
--- Decrease the inventory quantity count AFTER the transaction
-CREATE TRIGGER dec_inventory
-    AFTER INSERT
-    ON `transactions`
-    FOR EACH ROW
-BEGIN
-    DECLARE old_inventory_quantity INT;
-    DECLARE new_inventory_quantity INT;
-    SELECT quantity
-    INTO old_inventory_quantity
-    FROM inventory
-    WHERE inventory.ISBN = NEW.ISBN;
-
-    SELECT quantity - NEW.quantity
-    INTO new_inventory_quantity
-    FROM inventory
-    WHERE inventory.ISBN = NEW.ISBN;
-
-    IF new_inventory_quantity < 0 THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Error: Not enough inventory quantity for transaction';
-
-    ELSE
-        UPDATE `inventory`
-        SET quantity = new_inventory_quantity
-        WHERE ISBN = NEW.ISBN;
-    END IF;
+    UPDATE inventory
+    SET quantity = quantity - NEW.quantity
+    WHERE ISBN = NEW.ISBN;
+    UPDATE user_account
+    SET balance = balance - NEW.total_price
+    WHERE v_number = NEW.v_number;
 END;
